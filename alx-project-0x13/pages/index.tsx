0434a672d9578176b08b4a47c186d54d66f7f562
Latera Mekonnen <mekonnenlatera@gmail.com>
2025-07-03 18:50:05 +0300

    hooks final

--- alx-project-0x13/pages/index.tsx
+++ alx-project-0x13/pages/index.tsx
@@ -1,35 +1,27 @@
 import ImageCard from "@/components/common/ImageCard";
+import useFetchData from "@/hooks/useFetchData";
 import { ImageProps } from "@/interfaces";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 
 const Home: React.FC = () => {
   const [prompt, setPrompt] = useState<string>("");
   const [imageUrl, setImageUrl] = useState<string>("");
-  const [isLoading, setIsLoading] = useState<boolean>(false);
-   const [generatedImages, setGeneratedImages]= useState<ImageProps[]>([]);
-
-   const handleGenerateImage = async () => {
-    setIsLoading(true);
-    const resp = await fetch('/api/generate-image', {
-      method: 'POST',
-      body: JSON.stringify({  prompt   }),
-      headers: {
-        'Content-type': 'application/json'
-      }
-    })
+  const { isLoading, responseData, generatedImages, fetchData } = useFetchData<any, { prompt: string}>();
+ 
+ 
+ const handleGenerateImage =  () => {
+    fetchData('/api/generate-image', { prompt })
+  }
 
 
-    if (!resp.ok) {
-      setIsLoading(false)
-      return;
+  useEffect(() => {
+    if (!isLoading) {
+      setImageUrl(responseData?.message)
     }
+  }, [isLoading])
 
-    const data = await resp.json()
-        setIsLoading(false);
-        setImageUrl(data?.message);
-        setGeneratedImages((prev)=>[...prev,{ imageUrl: data?.message, prompt } ]);
-  };
+
 
   return (
     <div className="flex flex-col items-center min-h-screen bg-gray-100 p-4">
